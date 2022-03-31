c3bc7a91b6184fd2f15b3c20690ba3dd9b1d9a2b
mmubasher <mmubasher@citrusbits.com>
2022-03-31 15:38:44 +0500

    FP-1  #time 5m

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fooproject
 foo project
+
+
+# This is a sample project to test code review features
--- app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// sample file
-
-// This is another sample file
-
-// I have worked too much now
