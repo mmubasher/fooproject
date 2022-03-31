c96d5b88133a796575adc216facbc682c4f10979
mmubasher <mmubasher@citrusbits.com>
2022-03-31 16:31:54 +0500

    FP-1 #time 6m #comment please someone

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+for (let i = 100; i > 100; i++){
+    console.log(i, 'logger');
+}
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
